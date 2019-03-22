c2a3e038914ef980417045c15c066e715301d71c
joao <joao182cx@gmail.com>
2019-03-21 23:52:47 -0300

    Criação da classe Clube.

--- src/Clube.java
+++ src/Clube.java
@@ -4,5 +4,34 @@ public class Clube {
 	
 	private String nome;
 	private Color[] cores;
+	
+	public Clube(String nome, Color corUm, Color corDois, Color corTres) {
+		super();
+		this.nome = nome;
+		
+		cores = new Color[3];
+		
+		cores[0] = corUm;
+		cores[1] = corDois;
+		cores[2] = corTres;
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Color[] getCores() {
+		return cores;
+	}
+
+	public void setCores(Color[] cores) {
+		this.cores = cores;
+	}
+	
 
 }
